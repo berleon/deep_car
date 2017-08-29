1e09563e939e6dda7724aabbe2728d0fc8e7e1c1
berleon <github@leon-sixt.de>
2017-08-29 15:48:16 +0000

    Update documentation

--- README.md
+++ README.md
@@ -5,23 +5,26 @@ This is the repository for a software project at the Freie Universtität.
 
 # Dependencies
 
-See the `requirements.txt` file.
+See the `requirements.txt` file. The packages can be install with `$ pip install -r
+requirements.txt`.
 
 # Notebooks
 
 Most code is written in Jupyter Notebooks. Here is a short overview. A more
 detailed description can be found inside the notebooks.
 
-* [TODO: link notebooks with short description]
-* driver.ipynb
-* rosbag_to_hdf5.ipynb
-* train.ipynb
+1. [rosbag_to_hdf5.ipynb](notebooks/rosbag_to_hdf5.ipynb): Converts the rosbag
+   files to a train and test hdf5 file
+1. [train.ipynb](notebooks/train.ipynb): Train the neural network.
+1. [driver.ipynb](notebooks/driver.ipynb): Connects to the car, loads, and runs the
+   neural network live.
 
 # Code
 
-* `data.py`
-* `model.py`
-* `util.py`
+* [`data.py`](deep_car/data.py): Contains augmentation functions and other
+  helper functions
+* [`model.py`](deep_car/model.py): The neural network architecture is defined
+  here.
 
 # Train data collection
 
@@ -32,7 +35,7 @@ to driving in the same direction. In the end we had 3 big files with trainig
 data.
 For the second generation we placed various obstacles on the driving road.
 We used orange soccer balls and some big chess statues. This time we were also
-driving in both directions. The driving time was about 1 1/2 hour. Again the 
+driving in both directions. The driving time was about 1 1/2 hour. Again the
 driving was done manually with the android app. This time we split our trainig
 data into many smaller data sets.
 
@@ -40,8 +43,10 @@ Finally we combined the data to create our total test set.
 
 We used the following command for recording the data:
 
+```
    rosbag record  \
     	/manual_control/speed \
     	/manual_control/steering \
     	/model_car/yaw \
         /deepcar/resize_img80x60/compressed
+```
--- deep_car/model.py
+++ deep_car/model.py
@@ -3,88 +3,23 @@ import tensorflow as tf
 import numpy as np
 
 
-class LogisticMixture:
-    def __init__(self, pi_logit, mean, scale, min=0., max=1., num=100):
-        self.pi_logit = pi_logit
-        self.mean = mean
-        self.scale = scale
-        self.min = min
-        self.max = max
-        self.num = num
-
-    def log_p(self, x):
-        return log_p_x(x, self.pi_logit, self.mean, self.scale,
-                       self.min, self.max, self.num)
-
-    def log_p_pdf(self, x):
-        rv = tf.contrib.distributions.Logistic(self.mean, self.scale)
-        p = rv.prob(x)
-        return tf.reduce_logsumexp(tf.nn.log_softmax(self.pi_logit) +
-                                   tf.log(tf.maximum(p, 1e-12)), axis=-1)
-
-    def discrete_probability_distribution(self):
-        x = tf.linspace(self.min, self.max, self.num)
-        x = tf.expand_dims(x, axis=0)
-        x = tf.tile(x, [tf.shape(self.mean)[0], 1])
-        x = tf.expand_dims(x, axis=-1)
-        pi_logit = tf.tile(self.pi_logit, [1, self.num, 1])
-        mean = tf.tile(self.mean, [1, self.num, 1])
-        scale = tf.tile(self.scale, [1, self.num, 1])
-        return log_p_x(
-            x, pi_logit, mean, scale, min=self.min, max=self.max, num=self.num
-        )
-
-
-def log_p_x(x, pi_logits, means, s, min=-np.pi/2, max=np.pi/2, num=180):
-    """
-    bs - batch_size
-    w - width
-    k - number of components
-
-    x: (bs, w, 1)
-    mu, s, pi_logits: (bs, w, k)
-    """
-    delta = (max - min) / (2.*num)
-    # rv = tf.contrib.distributions.Logistic(means, s)
-    cdf = lambda t: tf.nn.sigmoid((t - means) / s)
-    p = cdf(x + delta) - cdf(x - delta)
-    k = pi_logits.get_shape()[-1]
-    x = tf.tile(x, [1, 1, int(k)])
-    p = tf.where(
-        x - delta <= min,
-        cdf(min),
-        p,
-    )
-    p = tf.where(
-        x + delta >= max,
-        1. - cdf(max),
-        p,
-    )
-    return tf.reduce_logsumexp(tf.nn.log_softmax(pi_logits) + tf.log(tf.maximum(p, 1e-12)), axis=-1)
-
-
 class Model:
-    def __init__(self, input_shape, n_mixtures=6, y_delta_buckets=2, reuse=False):
-        self.n_mixtures = n_mixtures
+    """Build the neural network"""
+
+    def __init__(self, input_shape, reuse=False):
         with tf.variable_scope("deep_car", reuse=reuse):
             # inputs
             self.image = tf.placeholder(tf.float32, shape=input_shape, name='image')
-            self.steering_abs = tf.placeholder(tf.float32, shape=[None, 1], name='steering_abs')
-            self.y_delta_buckets = y_delta_buckets
+            self.steering_abs_true = tf.placeholder(tf.float32, shape=[None, 1],
+                                                    name='steering_abs_true')
             # training flag for batch norm
             self.training = tf.placeholder_with_default(False, shape=[], name='training')
             # ground truth
-            self.y_distance_true = tf.placeholder(
-                    tf.float32, shape=[None, 1], name='y_distance_true')
-            self.y_delta_true = tf.placeholder(tf.int32, shape=[None, 1], name='y_delta_true')
-            self.y_delta_true_hot = tf.one_hot(self.y_delta_true, depth=y_delta_buckets)
-
-            self.setup_distance_prob()
-            self.setup_steering()
+            self.setup_model()
             self.setup_optimizer()
 
-    def setup_distance_prob(self):
-        f = 48
+    def setup_model(self):
+        f = 32
         k = 5
         # TODO: fix batch normalization
 
@@ -100,7 +35,6 @@ class Model:
         l = tf.nn.relu(l)
         l = tf.layers.max_pooling2d(l, 2, 2)
 
-        print(l.get_shape())
         # 12x16
         l = tf.layers.conv2d(l, 4*f, k, padding='same')
         l = tf.layers.batch_normalization(l, training=self.training)
@@ -112,54 +46,25 @@ class Model:
         l = tf.nn.relu(l)
         l = tf.layers.max_pooling2d(l, (2, 2), 2)
 
-        print(l.get_shape())
         # 6x8
         l = tf.layers.conv2d(l, 8*f, k, padding='same')
         l = tf.layers.batch_normalization(l, training=self.training)
         l = tf.nn.relu(l)
         l = tf.layers.average_pooling2d(l, (3, 4), 1)
         l = tf.contrib.layers.flatten(l)
-        l = tf.concat([l, self.steering_abs], axis=-1)
+
         l = tf.layers.dense(l, 4*f)
         l = tf.layers.batch_normalization(l, training=self.training)
-        l = tf.nn.relu(l)
-
-        print(l.get_shape())
-        self.nn_features = l
-
-        mu = .6*tf.tanh(tf.layers.dense(l, self.n_mixtures)) + 0.5
-        pi_logits = tf.layers.dense(l, self.n_mixtures)
-        s = 0.2*tf.nn.softplus(tf.layers.dense(l, self.n_mixtures))
-
-        mu = tf.expand_dims(mu, axis=1)
-        pi_logit = tf.expand_dims(pi_logits, axis=1)
-        s = tf.expand_dims(s, axis=1)
-        self.mixture = LogisticMixture(pi_logit, mu, s)
-
-        self.y_distance_prob = tf.identity(
-            self.mixture.discrete_probability_distribution(), name='y_distance_discr_prob')
+        self.steering_abs_pred = tf.identity(
+            np.pi*tf.tanh(tf.layers.dense(l, 1)), name='steering_abs_pred')
 
     def setup_optimizer(self):
-        self.y_distance_loss = tf.identity(
-            -self.mixture.log_p(tf.expand_dims(self.y_distance_true, axis=1)),
-            name='y_distance_loss')
-
-        self.y_delta_loss = tf.nn.softmax_cross_entropy_with_logits(
-            labels=self.y_delta_true_hot, logits=self.y_steering_logits,
-            name='y_delta_loss')
+        self.steering_abs_loss = tf.identity(
+           tf.reduce_mean((self.steering_abs_true - self.steering_abs_pred)**2)
+        )
 
         opt = tf.train.AdamOptimizer(learning_rate=0.0003)
         update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
         with tf.control_dependencies(update_ops):
             self.opt_op = opt.minimize(
-                self.y_delta_loss + self.y_distance_loss, name='opt')
-
-    def setup_steering(self):
-        f = 16
-        l = tf.concat([self.nn_features, self.y_distance_true], axis=-1)
-        l = tf.layers.dense(l, 4*f)
-        l = tf.layers.batch_normalization(l, training=self.training)
-        l = tf.nn.relu(l)
-        self.y_steering_logits = tf.layers.dense(l, 2)
-        self.y_steering_prob = tf.nn.softmax(self.y_steering_logits,
-                                             name='y_steering_prob')
+                self.steering_abs_loss, name='opt')
